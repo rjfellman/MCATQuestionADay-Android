924b08326502fb67e1febac455f8669884ae5c57
Russ Fellman <russ216@gmail.com>
2012-04-22 21:26:01 -0400

    Merging stats

--- src/com/mcatquestion/android/MyStats.java
+++ src/com/mcatquestion/android/MyStats.java
@@ -14,25 +14,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.webkit.WebView;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MyStats extends Activity{
 	
-	WebView barcharts, percentile;
+	ImageView barchart1, barchart2, barchart3, barchart4;
+	ImageView pctBG1,pctBG2,pctBG3,pctBG4;
+	ImageView pctMark1, pctMark2, pctMark3, pctMark4;
+	TextView barText1, barText2, barText3, barText4;
+	TextView pctText1, pctText2, pctText3, pctText4;
 	
 	public static final String myStatsUrl = "http://www.mcatquestionaday.com/iPhoneX/getStats.php?userid=";
-	public static final String barchartsURL= "http://www.mcatquestionaday.com/iPhoneX/MCAT-Bargraphs-Stats.html";
-	public static final String pctURL= "http://www.mcatquestionaday.com/iPhoneX/MCAT-Percentiles.html";
 	
 	/** Called when the activity is first created. */
     @Override
@@ -46,9 +43,205 @@ public class MyStats extends Activity{
         
         TextView userLabel = (TextView) findViewById(R.id.usernameStats);
         userLabel.setText("Stats for "+userID);
-                
-        //Intent achartIntent = new MyStatsChart().execute(this);
-        //startActivity(achartIntent);
+        
+        //get the text views
+        barText1 = (TextView) findViewById(R.id.TextView01);
+        barText2 = (TextView) findViewById(R.id.TextView09);
+        barText3 = (TextView) findViewById(R.id.TextView06);
+        barText4 = (TextView) findViewById(R.id.TextView07);
+        
+      //get the text views
+        pctText1 = (TextView) findViewById(R.id.TextView04);
+        pctText2 = (TextView) findViewById(R.id.TextView02);
+        pctText3 = (TextView) findViewById(R.id.TextView03);
+        pctText4 = (TextView) findViewById(R.id.textView1);
+        
+      //JSON
+        JSONObject json = getJSONfromURL(myStatsUrl + userID);
+        JSONArray jarray = null;
+
+        ;
+        ;
+        	
+        //set the text views
+        try {
+			barText1.setText(json.getString("PhysicsScore")+"%");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			barText2.setText(json.getString("BioScore")+"%");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			barText3.setText(json.getString("ChemScore")+"%");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			barText4.setText(json.getString("OrgoScore")+"%");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        //set the text views
+        try {
+			pctText1.setText("Physics - " + json.getString("PhysicsPercentile") + "th Percentile ");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			pctText2.setText("Biology - " + json.getString("BioPercentile") + "th Percentile");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			pctText3.setText("Chemistry - " + json.getString("ChemPercentile") + "th Percentile");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			pctText4.setText("Orgo - " + json.getString("OChemPercentile") + "th Percentile");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        //get the image views
+        barchart1 = (ImageView) findViewById(R.id.ImageView01);
+        barchart2 = (ImageView) findViewById(R.id.ImageView02);
+        barchart3 = (ImageView) findViewById(R.id.ImageView03);
+        barchart4 = (ImageView) findViewById(R.id.ImageView04);
+        
+        //get the image views
+        pctBG1 = (ImageView) findViewById(R.id.ImageView05);
+        pctBG2 = (ImageView) findViewById(R.id.ImageView06);
+        pctBG3 = (ImageView) findViewById(R.id.ImageView07);
+        pctBG4 = (ImageView) findViewById(R.id.imageView9);
+        
+        //get the image views
+        pctMark1 = (ImageView) findViewById(R.id.imageView1);
+        pctMark2 = (ImageView) findViewById(R.id.imageView10);
+        pctMark3 = (ImageView) findViewById(R.id.imageView11);
+        pctMark4 = (ImageView) findViewById(R.id.imageView2);
+        
+        /*
+        Bitmap bmp1=BitmapFactory.decodeResource(getResources(), R.drawable.horizbar);
+        int width1=20;
+        int height1=0;
+		try {
+			height1 = Integer.parseInt(json.getString("PhysicsScore"));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        Bitmap resizedbitmap1=Bitmap.createScaledBitmap(bmp1, width1, height1, true);
+
+        barchart1.setImageBitmap(resizedbitmap1);
+        
+        Bitmap bmp2=BitmapFactory.decodeResource(getResources(), R.drawable.horizbar);
+        int width2=20;
+        int height2=0;
+		try {
+			height2 = Integer.parseInt(json.getString("BioScore"));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        Bitmap resizedbitmap2=Bitmap.createScaledBitmap(bmp2, width2, height2, true);
+
+        barchart2.setImageBitmap(resizedbitmap2);
+        
+        Bitmap bmp3=BitmapFactory.decodeResource(getResources(), R.drawable.horizbar);
+        int width3=20;
+        int height3=0;
+		try {
+			height3 = Integer.parseInt(json.getString("ChemScore"));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        Bitmap resizedbitmap3=Bitmap.createScaledBitmap(bmp3, width3, height3, true);
+
+        barchart3.setImageBitmap(resizedbitmap3);
+        
+        Bitmap bmp4=BitmapFactory.decodeResource(getResources(), R.drawable.horizbar);
+        int width4=20;
+        int height4=0;
+		try {
+			height4 = Integer.parseInt(json.getString("OrgoScore"));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        Bitmap resizedbitmap4=Bitmap.createScaledBitmap(bmp4, width4, height4, true);
+
+        barchart4.setImageBitmap(resizedbitmap4);
+		*/
+        
         
     }
+    
+    public JSONObject getJSONfromURL(String url){
+
+		//initialize
+		InputStream is = null;
+		String result = "";
+		JSONObject jArray = null;
+
+		//http post
+		try{
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(url);
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+			is = entity.getContent();
+
+		}catch(Exception e){
+			Log.e("log_tag", "Error in http connection "+e.toString());
+		}
+
+		//convert response to string
+		try{
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+			result=sb.toString();
+		}catch(Exception e){
+			Log.e("log_tag", "Error converting result "+e.toString());
+		}
+
+		//try parse the string to a JSON object
+		try{
+	        	jArray = new JSONObject(result);
+		}catch(JSONException e){
+			Log.e("log_tag", "Error parsing data "+e.toString());
+		}
+
+		return jArray;
+	}
 }

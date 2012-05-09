e2c4bfa93b32a7dc5e6882b1604f79c1b18f02d0
Russ Fellman <russ216@gmail.com>
2012-05-08 22:37:40 -0400

    May 8

--- src/com/mcatquestion/android/MyStats.java
+++ src/com/mcatquestion/android/MyStats.java
@@ -1,11 +1,24 @@
 package com.mcatquestion.android;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.TextView;
 import android.view.MotionEvent;
 
 public class MyStats extends Activity{
@@ -15,6 +28,8 @@ public class MyStats extends Activity{
 	WebView bioPCT;
 	WebView ochemPCT;
 	WebView physicsScore,chemistryScore,biologyScore,orgoScore;
+	TextView barText1,barText2,barText3,barText4;
+	TextView pctText1,pctText2,pctText3,pctText4;
 	
 	/** Called when the activity is first created. */
     @Override
@@ -26,6 +41,73 @@ public class MyStats extends Activity{
         
         System.out.println(preferences.getUsername());
         Log.i("Test",preferences.getUsername());
+        
+      //get the text views
+        barText1 = (TextView) findViewById(R.id.barLabel1);
+        barText2 = (TextView) findViewById(R.id.barLabel3);
+        barText3 = (TextView) findViewById(R.id.barLabel2);
+        barText4 = (TextView) findViewById(R.id.barLabel4);
+        
+      //get the text views
+        pctText1 = (TextView) findViewById(R.id.pctLabel1);
+        pctText2 = (TextView) findViewById(R.id.pctLabel3);
+        pctText3 = (TextView) findViewById(R.id.pctLabel2);
+        pctText4 = (TextView) findViewById(R.id.pctLabel4);
+        
+      //JSON
+        JSONObject json = getJSONfromURL("http://www.mcatquestion.com/iPhoneX/getStats.php?userid="+preferences.getUsername());
+	
+        //set the text views
+        try {
+			barText1.setText(json.getString("PhysicsScore")+"%");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			barText2.setText(json.getString("BioScore")+"%");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			barText3.setText(json.getString("ChemScore")+"%");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			barText4.setText(json.getString("OrgoScore")+"%");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        //set the text views
+        try {
+			pctText1.setText("You are currently performing better than "+json.getString("PhysicsPercentile")+"% of all users");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			pctText2.setText("You are currently performing better than "+json.getString("BioPercentile")+"% of all users");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			pctText3.setText("You are currently performing better than "+json.getString("ChemPercentile")+"% of all users");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			pctText4.setText("You are currently performing better than "+json.getString("OrgoPercentile")+"% of all users");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         physicsScore = (WebView) findViewById(R.id.physicsScore);
         biologyScore = (WebView) findViewById(R.id.biologyScore);
@@ -99,4 +181,47 @@ public class MyStats extends Activity{
         ochemPCT.setBackgroundColor(0x00000000);
         
     }
+    
+    public JSONObject getJSONfromURL(String url){
+
+		//initialize
+		InputStream is = null;
+		String result = "";
+		JSONObject jArray = null;
+
+		//http post
+		try{
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(url);
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+			is = entity.getContent();
+
+		}catch(Exception e){
+			Log.e("log_tag", "Error in http connection "+e.toString());
+		}
+
+		//convert response to string
+		try{
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+			result=sb.toString();
+		}catch(Exception e){
+			Log.e("log_tag", "Error converting result "+e.toString());
+		}
+
+		//try parse the string to a JSON object
+		try{
+	        	jArray = new JSONObject(result);
+		}catch(JSONException e){
+			Log.e("log_tag", "Error parsing data "+e.toString());
+		}
+
+		return jArray;
+	}
 }
